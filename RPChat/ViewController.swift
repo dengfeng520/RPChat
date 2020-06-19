d0513530b307b4d385001d260b0facf3763a2140
dengfeng520 <deng_feng520@163.com>
2020-06-19 14:37:52 +0800

    国际化修改

--- RPChat/ViewController.swift
+++ RPChat/ViewController.swift
@@ -14,10 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        testLab.text = NSLocalizedString("测试", comment: "")
+        testLab.text = NSLocalizedString("test", comment: "")
     }
-
-
-    
 }
 
