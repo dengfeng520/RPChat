153482fc8d2f9d405aae63494ec9ecfc2481f7b6
dengfeng520 <deng_feng520@163.com>
2020-07-14 11:30:38 +0800

    add Tests

---
+++ RPChatUnitTests/RPChatUnitTests.swift
@@ -0,0 +1,39 @@
+//
+//  RPChatUnitTests.swift
+//  RPChatUnitTests
+//
+//  Created by rp.wang on 2020/7/14.
+//  Copyright © 2020 Beijing Physical Fitness Sport Science and Technology Co.,Ltd. All rights reserved.
+//
+
+import XCTest
+import RxTest
+import RxSwift
+import Foundation
+
+@testable import RPChat_iOS
+@testable import RPChatDataKit
+
+class RPChatUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ RPChatUnitTests/SignInViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewModelTests.swift
+//  RPChatUnitTests
+//
+//  Created by rp.wang on 2020/7/14.
+//  Copyright © 2020 Beijing Physical Fitness Sport Science and Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewModelTests: RPChatUnitTests {
+
+}
