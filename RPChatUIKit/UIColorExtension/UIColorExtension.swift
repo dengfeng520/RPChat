c5d4d868b6315d505af49eff6622eb0e3c242fb0
dengfeng520 <deng_feng520@163.com>
2020-10-13 17:49:42 +0800

    update code

--- RPChatUIKit/UIColorExtension/UIColorExtension.swift
+++ RPChatUIKit/UIColorExtension/UIColorExtension.swift
@@ -136,21 +136,21 @@ extension UIColor {
 }
 
 extension UIColor {
-    public class func hexadecimalColor(_ hexadecimal: String, _ alpha: CGFloat? = 1) -> UIColor {
+    public class func hexStringToColor(_ hexadecimal: String, _ alpha: CGFloat? = 1) -> UIColor {
         var cstr = hexadecimal.trimmingCharacters(in:  CharacterSet.whitespacesAndNewlines).uppercased() as NSString;
-        if(cstr.length < 6){
+        if cstr.length < 6 {
             return .clear
         }
-        if(cstr.hasPrefix("0X")) {
+        if cstr.hasPrefix("0X") {
             cstr = cstr.substring(from: 2) as NSString
         }
-        if(cstr.hasPrefix("#")){
+        if cstr.hasPrefix("#") {
             cstr = cstr.substring(from: 1) as NSString
         }
-        if(cstr.length != 6) {
-            return UIColor.clear
+        if cstr.length != 6 {
+            return .clear
         }
-        var range = NSRange.init()
+        var range = NSRange()
         range.location = 0
         range.length = 2
         let rStr = cstr.substring(with: range)
@@ -161,41 +161,10 @@ extension UIColor {
         var r :UInt32 = 0x0
         var g :UInt32 = 0x0
         var b :UInt32 = 0x0
-        Scanner.init(string: rStr).scanHexInt32(&r)
-        Scanner.init(string: gStr).scanHexInt32(&g)
-        Scanner.init(string: bStr).scanHexInt32(&b)
-        return UIColor.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: alpha)
-    }
-    
-    public class func hexStringToColor(_ hexadecimal: String) -> UIColor {
-        var cstr = hexadecimal.trimmingCharacters(in:  CharacterSet.whitespacesAndNewlines).uppercased() as NSString
-        if (cstr.length < 6) {
-            return UIColor.clear;
-        }
-        if (cstr.hasPrefix("0X")) {
-            cstr = cstr.substring(from: 2) as NSString
-        }
-        if (cstr.hasPrefix("#")) {
-            cstr = cstr.substring(from: 1) as NSString
-        }
-        if (cstr.length != 6) {
-            return .clear
-        }
-        var range = NSRange()
-        range.location = 0
-        range.length = 2
-        let rStr = cstr.substring(with: range)
-        range.location = 2;
-        let gStr = cstr.substring(with: range)
-        range.location = 4;
-        let bStr = cstr.substring(with: range)
-        var r :UInt32 = 0x0
-        var g :UInt32 = 0x0
-        var b :UInt32 = 0x0
-        Scanner.init(string: rStr).scanHexInt32(&r)
-        Scanner.init(string: gStr).scanHexInt32(&g)
-        Scanner.init(string: bStr).scanHexInt32(&b)
-        return UIColor.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: 1)
+        Scanner(string: rStr).scanHexInt32(&r)
+        Scanner(string: gStr).scanHexInt32(&g)
+        Scanner(string: bStr).scanHexInt32(&b)
+        return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: alpha ?? 1)
     }
 }
 
